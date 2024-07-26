1f678307feb4c508fa471e8a1dc0e1f7d3515a81
Billy Franscois <BillyFranscois@outlook.com>
2024-07-26 23:11:26 +0900

    Update sql console

--- wwwroot/sql/console.session.sql
+++ wwwroot/sql/console.session.sql
@@ -2,6 +2,9 @@ SELECT * FROM Patients;
 
 SELECT * FROM Admin;
 
+DELETE FROM Admin WHERE Username = 'billyfrcs';
+
 DROP TABLE IF EXISTS __EFMigrationsHistory;
 
-DROP TABLE IF EXISTS Admin, Patients, __EFMigrationsHistory;
+DROP TABLE IF EXISTS Admin, Patients, __EFMigrationsHistory,
+AspNetRoleClaims, AspNetRoles, AspNetUserClaims, AspNetUserLogins, AspNetUserRoles, AspNetUsers, AspNetUserTokens;
